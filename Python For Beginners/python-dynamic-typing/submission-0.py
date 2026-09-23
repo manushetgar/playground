415f6d72665a1a04ee43e5bd2890c796fa79fd65
Manu Shetgar <49717516+manushetgar@users.noreply.github.com>
2026-09-23 22:24:18 +0200

    Add: python-dynamic-typing - submission-0

---
+++ Python For Beginners/python-dynamic-typing/submission-0.py
@@ -0,0 +1,10 @@
+variable = 10
+print(type(variable))
+variable = 3.90
+print(type(variable))
+variable = True
+print(type(variable))
+variable = "Keep learning"
+print(type(variable))
+variable = [3,4,5]
+print(type(variable))
